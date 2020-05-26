aac16b4ee7ec74cade25833108652f8a7b3a24c0
techievivek <techievivek123@gmail.com>
2020-05-26 20:07:22 +0530

    added models for usermap and caching workshop

--- createcourse/models.py
+++ createcourse/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UserMap(models.Model):
+    workshopUsername=models.CharField()
+    yakshUsername=models.CharField()
+class WorkshopCached(models.Model):
+    id=models.IntegerField()
