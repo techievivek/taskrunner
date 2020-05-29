3edeec2f2016fde027f9bd48b6f5f529ba62754d
techievivek <techievivek123@gmail.com>
2020-05-29 12:27:10 +0530

    formatted files

--- createcourse/tasks.py
+++ createcourse/tasks.py
@@ -7,20 +7,13 @@ import requests
 
 from requests.exceptions import HTTPError
 
-from .models import WorkshopCached,UserMap
+from .models import WorkshopCached, UserMap
 
 import json
 
 import datetime
 
-# @periodic_task(
-#     run_every=(
-#         crontab(
-#             hour='*', minute='*/1', day_of_week='*', day_of_month='*',
-#             month_of_year='*'
-#         )
-#     ), name='Fetch data from workshop site'
-# )
+
 @shared_task
 def fetch_data():
     url = 'localhost:5000'
@@ -29,9 +22,9 @@ def fetch_data():
         response = requests.get(url, params=params)
         response.raise_for_status()
         response_data = response.json()  #Json_data_received
-        workshop_id = response_data['id'] #later used for caching
-        workshop_username = response_data['instructor'] #for user mapping
-        Userobj=UserMap.objects.get(workshopUsername=workshop_username)
+        workshop_id = response_data['id']  #later used for caching
+        workshop_username = response_data['instructor']  #for user mapping
+        Userobj = UserMap.objects.get(workshopUsername=workshop_username)
         #get the yaksh username from this.
         #prepare the data and send the post request
         post_url = r'http://localhost:8000/api/course/create/'
@@ -39,18 +32,24 @@ def fetch_data():
             with open('demo-data.json') as f:
                 json_data = json.load(f)
             #prepare the json_data to be sent to post api.
-            yaksh_user=Userobj.yayakshUsername
-            course_info={}
-            course_info['name']=response_data['name'] #course name
-            course_info['creator']=yaksh_user #course creator
-            course_info['created_on']=response_data['date'] #course created on
-            course_info['start_enroll_time']=response_data['date'] #course start date
-            course_duration=response_data['duration']
-            course_duration_days=course_duration.find('days')
-            course_duration=int(course_duration[:course_duration_days])
-            course_info['end_enroll_time']=response_data['date']+datetime.timedelta (days=course_duration) #course end date
+            yaksh_user = Userobj.yayakshUsername
+            course_info = {}
+            course_info['name'] = response_data['name']  #course name
+            course_info['creator'] = yaksh_user  #course creator
+            course_info['created_on'] = response_data[
+                'date']  #course created on
+            course_info['start_enroll_time'] = response_data[
+                'date']  #course start date
+            course_duration = response_data['duration']
+            course_duration_days = course_duration.find('days')
+            course_duration = int(course_duration[:course_duration_days])
+            course_info['end_enroll_time'] = response_data[
+                'date'] + datetime.timedelta(
+                    days=course_duration)  #course end date
             #Course basic info data is prepared now join with learning module
-            course_info.update(json_data) #learnin module data is appended in course_info dict
+            course_info.update(
+                json_data
+            )  #learnin module data is appended in course_info dict
             post_response = requests.post(post_url, json=course_info)
             if post_response:
                 #update the workshop cached with a new entry.
@@ -69,4 +68,3 @@ def fetch_data():
         print(f'Other error occurred: {err}')  # Python 3.6
     else:
         print('Request Success!')  #The request was successful.
-
--- masterapp/settings.py
+++ masterapp/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'v^iso#&$lb*q#&$$g@-07b@huu-ml3fa54zea^_baj(%@hg#+6'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'masterapp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -82,25 +79,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -114,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
