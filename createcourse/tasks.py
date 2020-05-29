ba705caa1528369f5069092643dffcee19695980
techievivek <techievivek123@gmail.com>
2020-05-29 14:05:13 +0530

    removed extra try,except block

--- createcourse/tasks.py
+++ createcourse/tasks.py
@@ -28,40 +28,33 @@ def fetch_data():
         #get the yaksh username from this.
         #prepare the data and send the post request
         post_url = r'http://localhost:8000/api/course/create/'
-        try:
-            with open('demo-data.json') as f:
-                json_data = json.load(f)
-            #prepare the json_data to be sent to post api.
-            yaksh_user = Userobj.yayakshUsername
-            course_info = {} 
-            course_info['name'] = response_data['name']  #course name
-            course_info['creator'] = yaksh_user  #course creator
-            course_info['created_on'] = response_data[
-                'date']  #course created on
-            course_info['start_enroll_time'] = response_data[
-                'date']  #course start date
-            course_duration = response_data['duration']
-            course_duration_days = course_duration.find('days')
-            course_duration = int(course_duration[:course_duration_days])
-            course_info['end_enroll_time'] = response_data[
-                'date'] + datetime.timedelta(
-                    days=course_duration)  #course end date
-            #Course basic info data is prepared now join with learning module
-            course_info.update(
-                json_data
-            )  #learnin module data is appended in course_info dict
-            post_response = requests.post(post_url, json=course_info)
-            if post_response:
-                #update the workshop cached with a new entry.
-                WorkshopCached.objects.create(id=workshop_id)
-            # If the response was successful, no Exception will be raised
-            post_response.raise_for_status()
-        except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')  # Python 3.6
-        except Exception as err:
-            print(f'Other error occurred: {err}')  # Python 3.6
+        with open('demo-data.json') as f:
+            json_data = json.load(f)
+        #prepare the json_data to be sent to post api.
+        yaksh_user = Userobj.yayakshUsername
+        course_info = {} 
+        course_info['name'] = response_data['name']  #course name
+        course_info['creator'] = yaksh_user  #course creator
+        course_info['created_on'] = response_data[
+            'date']  #course created on
+        course_info['start_enroll_time'] = response_data[
+            'date']  #course start date
+        course_duration = response_data['duration']
+        course_duration_days = course_duration.find('days')
+        course_duration = int(course_duration[:course_duration_days])
+        course_info['end_enroll_time'] = response_data[
+            'date'] + datetime.timedelta(
+                days=course_duration)  #course end date
+        #Course basic info data is prepared now join with learning module
+        course_info.update(
+            json_data
+        )  #learnin module data is appended in course_info dict
+        post_response = requests.post(post_url, json=course_info)
+        if post_response:
+            #update the workshop cached with a new entry.
+            WorkshopCached.objects.create(id=workshop_id)
         else:
-            print('Request Success!')  #The request was successful.
+            print("Something went wrong during post request.")
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')  # Python 3.6
     except Exception as err:
