b45a5460b016ae67531d67014508f367b7443071
techievivek <techievivek123@gmail.com>
2020-05-29 14:05:52 +0530

    formatted task file

--- createcourse/tasks.py
+++ createcourse/tasks.py
@@ -21,7 +21,8 @@ def fetch_data():
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()
-        response_data = response.json()  #Data recieved and converted into dict.
+        response_data = response.json(
+        )  #Data recieved and converted into dict.
         workshop_id = response_data['id']  #later used for caching
         workshop_username = response_data['instructor']  #for user mapping
         Userobj = UserMap.objects.get(workshopUsername=workshop_username)
@@ -32,23 +33,21 @@ def fetch_data():
             json_data = json.load(f)
         #prepare the json_data to be sent to post api.
         yaksh_user = Userobj.yayakshUsername
-        course_info = {} 
+        course_info = {}
         course_info['name'] = response_data['name']  #course name
         course_info['creator'] = yaksh_user  #course creator
-        course_info['created_on'] = response_data[
-            'date']  #course created on
+        course_info['created_on'] = response_data['date']  #course created on
         course_info['start_enroll_time'] = response_data[
             'date']  #course start date
         course_duration = response_data['duration']
         course_duration_days = course_duration.find('days')
         course_duration = int(course_duration[:course_duration_days])
-        course_info['end_enroll_time'] = response_data[
-            'date'] + datetime.timedelta(
+        course_info[
+            'end_enroll_time'] = response_data['date'] + datetime.timedelta(
                 days=course_duration)  #course end date
         #Course basic info data is prepared now join with learning module
         course_info.update(
-            json_data
-        )  #learnin module data is appended in course_info dict
+            json_data)  #learnin module data is appended in course_info dict
         post_response = requests.post(post_url, json=course_info)
         if post_response:
             #update the workshop cached with a new entry.
@@ -59,5 +58,3 @@ def fetch_data():
         print(f'HTTP error occurred: {http_err}')  # Python 3.6
     except Exception as err:
         print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Request Success!')  #The request was successful.
