87e539d37930964e646b818c3827fb70c849a8ab
techievivek <techievivek123@gmail.com>
2020-06-08 21:09:05 +0530

    added tests for tasks

--- createcourse/tests.py
+++ createcourse/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django_celery_results.models import TaskResult
 # Create your tests here.
+from .tasks import fetch_data
+
+
+class TestFetchDataTask(TestCase):
+    def setUp(self):
+        self.task = fetch_data.delay()
+
+    def test_task_state(self):
+        self.assertEqual(self.task.state, 'SUCCESS')
+
+    def test_task_result(self):
+        t = TaskResult.objects.order_by('-date_done').first()
+        self.assertEqual(t.status, 'SUCCESS')
+
+    def clean_up_models(self):
+        _=TaskResult.objects.order_by('-date_done').first().delete()
